33ba7fa8179e94da9e8e8147c33a2fb08df840ad
Cristian Vargas <cristian.v0223@gmail.com>
2023-06-07 11:30:34 -0500

    why is vercel not deploying my most recent commits

--- pages/scenes/cta/index.tsx
+++ pages/scenes/cta/index.tsx
@@ -21,15 +21,15 @@ const CTA = () => {
                   <dl className="grid max-w-screen-md gap-8 lg:gap-32 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
                       <div className="flex flex-col items-center justify-center">
                           <dt className="mb-2 text-3xl text-[#011C43] md:text-4xl lg:text-5xl font-extrabold">73M+</dt>
-                          <dd className="font-light lg:text-[24px] text-[#011C43]">developers</dd>
+                          <dd className="font-light lg:text-[24px] text-[#011C43]">Developers</dd>
                       </div>
                       <div className="flex flex-col items-center justify-center">
                           <dt className="mb-2 text-3xl text-[#011C43] md:text-4xl lg:text-5xl font-extrabold">1B+</dt>
-                          <dd className="font-light lg:text-[24px] text-[#011C43]">contributors</dd>
+                          <dd className="font-light lg:text-[24px] text-[#011C43]">Contributors</dd>
                       </div>
                       <div className="flex flex-col items-center justify-center">
                           <dt className="mb-2 text-3xl text-[#011C43] md:text-4xl lg:text-5xl font-extrabold">4M+</dt>
-                          <dd className="font-light lg:text-[24px] text-[#011C43]">organizations</dd>
+                          <dd className="font-light lg:text-[24px] text-[#011C43]">Organizations</dd>
                       </div>
                   </dl>
               </div>
